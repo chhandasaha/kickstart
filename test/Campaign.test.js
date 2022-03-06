42b51c012c3d3fca62389b6b9fb466fac5041f54
Chhanda Saha <chhandasaha98@gmail.com>
2022-03-06 12:24:25 +0530

    changed in campaign test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,12 +1,12 @@
 //Test file to test our campaign
-import * as data from "../ethereum/build/CampaignFactory.json"
-import * as data from "../ethereum/build/Campaign.json"
+// import * as data from "../ethereum/build/CampaignFactory.json"
+// import * as data from "../ethereum/build/Campaign.json"
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-//const compiledFactory = require('../ethereum/build/CampaignFactory.json');
-//const compiledCampaign = require('../ethereum/build/Campaign.json');
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
 
 let accounts;
 let factory; // reference to the deployed instance of the factory
